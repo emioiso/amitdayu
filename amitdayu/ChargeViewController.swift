18fff294b02bbb3be061c5ddee919dda14c93ce9
emioiso <jijjjiijiijijiji@gmail.com>
2024-04-24 17:48:38 +0900

    ViewControllerのファイルを作成しました

---
+++ amitdayu/ChargeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChargeViewController.swift
+//  amitdayu
+//
+//  Created by emi oiso on 2024/04/24.
+//
+
+import UIKit
+
+class ChargeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
