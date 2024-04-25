dc1500a3fdc783575eebfc243feab3bb4820937d
emioiso <jijjjiijiijijiji@gmail.com>
2024-04-25 19:32:01 +0900

    ChargeViewControllerの決定ボタンを押したらコンソールに0を表示できました。(ボタンが動作するか確認済み)

--- amitdayu/ChargeViewController.swift
+++ amitdayu/ChargeViewController.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class ChargeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            // Do any additional setup after loading the view.
+       
     }
+        
+    @IBAction func btnAction(sender: UIButton){
+        print(sender.tag)
+    }
+        
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+       
+    
+    
+    
 }
