bf43df61cbe0fe4ce66a5ff2a2717e95cc95f489
emioiso <jijjjiijiijijiji@gmail.com>
2024-04-26 18:48:16 +0900

    テキストフィールドに文字列を記入し、決定ボタンを押された際にテキストフィールドに入力された文字列が"amidayuiiyudana" の場合、コンソールに"成功"と出力できました。

--- amitdayu/ChargeViewController.swift
+++ amitdayu/ChargeViewController.swift
@@ -9,14 +9,26 @@ import UIKit
 
 class ChargeViewController: UIViewController {
     
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
             // Do any additional setup after loading the view.
        
     }
-        
     @IBAction func btnAction(sender: UIButton){
         print(sender.tag)
+        // テキストフィールドの内容を取得
+                if let text = textField.text {
+                    // 文字列が "amidayuiiyudana" と一致するか確認
+                    if text == "amidayuiiyudana" {
+                        print("成功") // 一致した場合にコンソールに出力
+                    } else {
+                        print("不一致") // 一致しない場合
+                    }
+                } else {
+                    print("テキストフィールドが空") // テキストフィールドが空の場合
+                }
     }
         
     
